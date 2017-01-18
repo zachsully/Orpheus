efd3c66a8cdcb886a24809aa7ac829487546fa99
Zach Sullivan <zachsully@gmail.com>
2017-01-18 15:16:12 -0800

    add Hakaru versions of data

--- haskell/Orpheus/Data/Music.hs
+++ haskell/Orpheus/Data/Music.hs
@@ -14,32 +14,63 @@
 
 module Orpheus.Data.Music where
 
+import Language.Hakaru.Types.DataKind
+
+{-
+  We need two representations, one in Haskell and one in Hakaru
+-}       
+--------------------------------------------
 data Pitchclass = A | B | C | D | E | F | G
   deriving (Show,Eq,Ord,Enum)
 
+type HPitchclass =
+  'HData ('TyCon "Pitchclass") '[ '[], '[], '[], '[], '[], '[], '[] ]
+                                {- A |  B |  C |  D |  E |  F |  G -}
+
+
+
+--------------------------------------------
 data Accidental
   = Sharp
   | Flat
   | Natural
   deriving (Show,Eq,Ord,Enum)
 
+type HAccidental
+  = 'HData ('TyCon "Accidental") '[ '[], '[], '[] ]
+                             {- Sharp | Flat | Natural -}
+
+
+
+--------------------------------------------
 data Duration
-  = Breve
-  | Semibreve
-  | Minim
-  | Crotchet
-  | Quaver
-  | Semiquaver
-  | Demisemiquaver
+  = Whole
+  | Half Duration
+  | Dot  Duration
   deriving (Show,Eq,Ord,Enum)
 
+type HDuration
+  = 'HData ('TyCon "Duration") '[ '[] , '[ 'I ] , '[ 'I ] ]
+                              {- Whole |   H D  |    D D -}
+
+
+--------------------------------------------
 data Primitive
   = Note Pitchclass [Accidental] Int Duration -- the Int is the octive
   | Rest Duration
   deriving (Show,Eq,Ord)
 
+type HPrimitive
+  = 'HData ('TyCon "Primitive") '[ '[ HPitchclass, HList HAccidental, 'HInt, HDuration ] , '[ HDuration ] ]
+                                 {-                    Note                              |      Rest     -}
+
+--------------------------------------------
 data Music
   = Prim Primitive
   | Seq  Music Music
   | Par  Music Music
   deriving (Show,Eq,Ord)
+
+type HMusic
+  = 'HData ('TyCon "Music") '[ '[ HPrimitive ] , '[ 'I, 'I ] , '[ 'I, 'I ] ]
+                             {-    Prim        |   Seq M M   |  Par M M   -}
