a1f8c94592180fb50d21d3f1c9c250fff8aad265
Zach Sullivan <zachsully@gmail.com>
2017-02-03 12:29:23 -0800

    different nix files for development and packaging

--- default.nix
+++ default.nix
@@ -1,26 +1,18 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-
-let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, base, hakaru, parsec, pretty, stdenv }:
-      mkDerivation {
-        pname = "orpheus";
-        version = "0.0.1.0";
-        src = ./.;
-        libraryHaskellDepends = [ base hakaru parsec pretty ];
-        homepage = "zachsully.com";
-        description = "Music analysis";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage f {};
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+{ mkDerivation, alex, array, base, hakaru, happy, mtl, mwc-random
+, parsec, pretty, stdenv, vector
+}:
+mkDerivation {
+  pname = "orpheus";
+  version = "0.0.1.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array base hakaru mtl mwc-random parsec pretty vector
+  ];
+  libraryToolDepends = [ alex happy ];
+  executableHaskellDepends = [ base hakaru mwc-random pretty ];
+  homepage = "zachsully.com";
+  description = "Music analysis";
+  license = stdenv.lib.licenses.bsd3;
+}
