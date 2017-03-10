a7a8a1d838f81cd3b268f79295973b66dc3c72f4
Zach Sullivan <zachsully@gmail.com>
2017-03-10 02:20:23 -0800

    working version of the MusicXML parser, have not tested it on the whole dataset yet though

--- haskell/Data/MusicXML/Parser.hs
+++ haskell/Data/MusicXML/Parser.hs
@@ -1,35 +1,74 @@
 {-# LANGUAGE Arrows #-}
 module Data.MusicXML.Parser where
 
-import Data.Vector (fromList, Vector)
+import Data.Vector (fromList, singleton)
 import Orpheus.Data.Music.Diatonic
 import Text.XML.HXT.Core
 
 --------------------------------------------------------------------------------
 parseMusicXMLFile :: FilePath -> IO Score
 parseMusicXMLFile fp = do
-  score <- runX $ readDocument [] fp
-  return (Score $ fromList [])
+  score <- runX $ readDocument [ withValidate         False
+                               , withSubstDTDEntities False
+                               , withRemoveWS         True
+                               ] fp
+              >>> arrScorePartwise
+  case score of
+    []     -> error "Failed to parse score"
+    (s:[]) -> return s
+    _ -> error $ show $ score
 
 --------------------------------------------------------------------------------
 
-procScorePartwise :: (ArrowXml a) => a XmlTree Score
-procScorePartwise = getChildren >>> hasName "score-partwise" >>> undefined
+arrScorePartwise :: (ArrowXml a) => a XmlTree Score
+arrScorePartwise = arr id /> hasName "score-partwise"
+               >>> listA arrPart
+               >>> arr (Score . fromList)
 
-procNumParts :: (ArrowXml a) => a XmlTree Int
-procNumParts = getChildren >>> hasName "part-list" >>> undefined
+arrPart :: (ArrowXml a) => a XmlTree Voice
+arrPart = arr id /> hasName "part"
+      >>> listA arrMeasure
+      >>> arr (Voice . singleton . fromList . concat)
 
-procPart :: (ArrowXml a) => a XmlTree Voice
-procPart = getChildren >>> hasName "part" >>> undefined
+arrMeasure :: (ArrowXml a) => a XmlTree [Primitive]
+arrMeasure = arr id /> hasName "measure"
+         >>> listA arrNote
 
-procNote :: (ArrowXml a) => a XmlTree Primitive
-procNote = getChildren >>> hasName "note" >>> undefined
+arrNote :: (ArrowXml a) => a XmlTree Primitive
+arrNote = arr id /> hasName "note"
+      >>> (arrPitch &&& arrDuration)
+      >>> arr (\((p,oct), (dur,dot)) ->
+                Note p mempty cMajor commonTime oct dur dot)
 
-procPitchclass :: (ArrowXml a) => a XmlTree Pitchclass
-procPitchclass = getChildren >>> hasName "step" >>> undefined
+arrPitch :: (ArrowXml a) => a XmlTree (Pitchclass,Int)
+arrPitch = arr id /> hasName "pitch"
+       >>> (arrPitchclass &&& arrOctave)
 
-procOctave :: (ArrowXml a) => a XmlTree Int
-procOctave = getChildren >>> hasName "octave" >>> undefined
+arrPitchclass :: (ArrowXml a) => a XmlTree Pitchclass
+arrPitchclass = arr id /> hasName "step" /> getText
+            >>> arr (\s -> case s of
+                            "A" -> A
+                            "B" -> B
+                            "C" -> C
+                            "D" -> D
+                            "E" -> E
+                            "F" -> F
+                            "G" -> G
+                            _   -> error $ "Unrecognized pitchclass: " ++ s)
 
-procDuration :: (ArrowXml a) => a XmlTree Duration
-procDuration = getChildren >>> hasName "duration" >>> undefined
+arrOctave :: (ArrowXml a) => a XmlTree Int
+arrOctave = arr id /> hasName "octave" /> getText
+        >>> arr (read :: String -> Int)
+
+-- duration depends of divisions tag, which we will ignore here
+arrDuration :: (ArrowXml a) => a XmlTree (Duration,Bool)
+arrDuration = arr id /> hasName "duration" /> getText
+          >>> arr (\s -> case s of
+                          "16"  -> (DWhole,False)
+                          "8"   -> (Whole,False)
+                          "6"   -> (Half,True)
+                          "4"   -> (Half,False)
+                          "3"   -> (Quarter,True)
+                          "2"   -> (Quarter,False)
+                          "1"   -> (Eighth,False)
+                          _     -> error $ "Unrecognized duration: " ++ s)
