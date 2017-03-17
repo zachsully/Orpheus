96e28d4ee8914331693060b3c43ae1172335ebf5
Zach Sullivan <zachsully@gmail.com>
2017-03-16 17:06:10 -0700

    add feature extraction file and remove trash

---
+++ haskell/Orpheus/Data/Feature.hs
@@ -0,0 +1,13 @@
+module Orpheus.Data.Feature where
+
+import Orpheus.DataSet
+import Orpheus.Data.Music.Context       
+
+uniqueKeySig :: DataSet -> [KeySig]
+uniqueKeySig = undefined
+
+uniqueTimeSig :: DataSet -> [TimeSig]
+uniqueTimeSig = undefined
+
+uniquePrimitive :: DataSet -> [Primitive]              
+uniquePrimitive = undefined
