98bfc9d7a4d751397d9385136dcfafb7932927be
Zach Sullivan <zachsully@gmail.com>
2017-03-20 19:00:18 -0700

    fleshing out paper, final version of slides

--- python/util.py
+++ python/util.py
@@ -6,7 +6,7 @@ feature_files = ["dataset/feature/timesig.csv",
                  "dataset/feature/primitive.csv",
                  "dataset/feature/all.csv"]
 
-def read_data(filename):
+def read_data_n(filename):
   f = open(filename, 'r')
   p = re.compile(',')
   feature_data = []
@@ -20,7 +20,7 @@ def read_data(filename):
     label_data.append(y)
   return (feature_data,label_data)
 
-def read_data_2(filename):
+def read_data_h(filename):
   f = open(filename, 'r')
   p = re.compile(',')
   feature_data = []
@@ -30,7 +30,25 @@ def read_data_2(filename):
     example = [int(x) for x in p.split(l.strip())]
     x = example[0:-1]
     y = example[-1]
-    print (x,y)
+    if y is 3:
+      for i in range(5):
+        feature_data.append(x)
+        label_data.append(y)
+    else:
+      feature_data.append(x)
+      label_data.append(y)
+  return (feature_data,label_data)
+
+def read_data(filename):
+  f = open(filename, 'r')
+  p = re.compile(',')
+  feature_data = []
+  label_data = []
+  namehash = {}
+  for l in f:
+    example = [int(x) for x in p.split(l.strip())]
+    x = example[0:-1]
+    y = example[-1]
     if y is 1:
       r = uniform(0,1)
       if r < 0.33:
@@ -40,10 +58,6 @@ def read_data_2(filename):
       for i in range(3):
         feature_data.append(x)
         label_data.append(y)
-    # elif y is 3:
-    #   for i in range(5):
-    #     feature_data.append(x)
-    #     label_data.append(y)
     else:
       feature_data.append(x)
       label_data.append(y)
